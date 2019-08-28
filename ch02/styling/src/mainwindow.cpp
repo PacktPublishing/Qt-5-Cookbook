5ee9b3eda0340de85f491a9712aea85e7208bf48
Sandro S. Andrade <sandroandrade@kde.org>
2019-08-28 17:21:41 -0300

    Final review of ch02/styling

--- ch02/styling/src/mainwindow.cpp
+++ ch02/styling/src/mainwindow.cpp
@@ -10,8 +10,7 @@
 #include <QToolBar>
 #include <QVBoxLayout>
 
-MainWindow::MainWindow(QWidget *parent) :
-   QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow {parent}
 {
    // Set window attributes
    setWindowTitle(tr("Styling Example Application"));
@@ -27,48 +26,48 @@ MainWindow::MainWindow(QWidget *parent) :
       QApplication::instance(), &QApplication::exit);
    auto alignMenu = menuBar()->addMenu(tr("&Align"));
    alignMenu->addActions({
-                            new QAction { tr("Align &left") },
-                            new QAction { tr("Align &center") },
-                            new QAction { tr("Align &right") }
+                            new QAction {tr("Align &left")},
+                            new QAction {tr("Align &center")},
+                            new QAction {tr("Align &right")}
                          });
    alignMenu->addSeparator();
    alignMenu->addActions({
-                            new QAction { tr("Align &top") },
-                            new QAction { tr("Align ce&nter") },
-                            new QAction { tr("Align &bottom") }
+                            new QAction {tr("Align &top")},
+                            new QAction {tr("Align ce&nter")},
+                            new QAction {tr("Align &bottom")}
                          });
 
    // Main toolbar with simple action
    auto mainToolBar = addToolBar(tr("Main toolbar"));
-//   mainToolBar->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
    mainToolBar->addActions(fileMenu->actions());
    mainToolBar->addSeparator();
-   mainToolBar->addAction(tr("&Alignment Large Text"))->setMenu(alignMenu);
+   mainToolBar->addAction(
+            tr("&Alignment Large Text"))->setMenu(alignMenu);
 
    // Central widget
    auto centralWidget = new QWidget;
-   auto layout = new QVBoxLayout { centralWidget };
+   auto layout = new QVBoxLayout {centralWidget};
 
    // ComboBox and Slider
    auto horizontalLayout1 = new QHBoxLayout;
    auto comboBox = new QComboBox;
-   comboBox->setItemDelegate(new QStyledItemDelegate { comboBox });
-   comboBox->addItems({ tr("One"), tr("Two"), tr("Three"), tr("Four")});
+   comboBox->setItemDelegate(new QStyledItemDelegate {comboBox});
+   comboBox->addItems({tr("One"), tr("Two"), tr("Three"), tr("Four")});
    horizontalLayout1->addWidget(comboBox);
-   auto slider = new QSlider { Qt::Horizontal };
+   auto slider = new QSlider {Qt::Horizontal};
    slider->setSizePolicy(comboBox->sizePolicy());
    horizontalLayout1->addWidget(slider);
    layout->addLayout(horizontalLayout1);
 
    auto horizontalLayout2 = new QHBoxLayout;
-   horizontalLayout2->addWidget(new QLabel { tr("A lineedit: ") });
+   horizontalLayout2->addWidget(new QLabel {tr("A lineedit: ")});
    horizontalLayout2->addWidget(new QLineEdit);
    layout->addLayout(horizontalLayout2);
 
    setCentralWidget(centralWidget);
 
    // Status bar
-   statusBar()->addPermanentWidget(new QLabel { tr("Ready!") }, 1);
+   statusBar()->addPermanentWidget(new QLabel {tr("Ready!")}, 1);
 
    setAttribute(Qt::WA_TranslucentBackground);
    setWindowFlags(Qt::FramelessWindowHint);
